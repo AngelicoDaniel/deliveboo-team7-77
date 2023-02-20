c2e06f0d616ccaf0551a366dedce9bffc3b164cf
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-02-20 10:02:30 +0100

    mod testo index

--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>ristoranti</h1>
+    <h1>Il Tuo ristorante</h1>
    <p>Nome del tuo ristorante:  {{$user->name}}</p>
    <p>Indirizzo del tuo ristorante:  {{$user->address}}</p>
    <p>P.iva del tuo ristorante:  {{$user->vat}}</p>
