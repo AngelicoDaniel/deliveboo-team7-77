83491b306abd30d05972a5944605d6b6144dd896
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-02-22 12:22:59 +0100

    Font awesome aggiunto

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.app')
 @section('content')
-
     <h1>Lista piatti:</h1>
     <ol>
         @foreach ($dishes as $dish)
-            <li> <a href="{{route('admin.dishes.show',['dish'=>$dish])}}">{{$dish->name}}</a>
+            <li> <a href="{{ route('admin.dishes.show', ['dish' => $dish]) }}">{{ $dish->name }}</a>
                 <ul>
-                    <li>{{$dish->price}} €</li>
-                    <li><img src="{{$dish->image}}" alt=""></li>
-                    <form action="{{ route('admin.dishes.destroy', $dish->id)}}" method="POST">
+                    <li>{{ $dish->price }} €</li>
+                    <li><img src="{{ $dish->image }}" alt=""></li>
+                    <form action="{{ route('admin.dishes.destroy', $dish->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
-                            <Button type="submit" class="btn btn-danger">
-                                <i class="fa-solid fa-trash"></i>
-                            </Button>
-                        </form>
-                        <form>
-                            <a href="{{ route('admin.dishes.edit', $dish->id)}}"><i class="fa-solid fa-pencil"></i>EDIT</a>
-                        </form>
+                        <Button type="submit" class="btn btn-danger">
+                            <i class="fa-solid fa-trash"></i>
+                        </Button>
+                    </form>
+                    <form>
+
+                        <button class="btn btn-success"><a class="text-white"
+                                href="{{ route('admin.dishes.edit', $dish->id) }}"><i
+                                    class="fa-regular fa-pen-to-square"></i></a></button>
+                    </form>
                 </ul>
             </li>
         @endforeach
     </ol>
-
 @endsection
