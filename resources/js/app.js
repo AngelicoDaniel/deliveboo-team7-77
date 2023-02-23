dd8a3e2ef00f7d4bdf5236176b9544f4f9aed47d
Daniel <angelicodaniel93@gmail.com>
2023-02-23 14:35:40 +0100

    allucinante

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,31 @@
-require('./bootstrap');
-window.Vue = require('vue');
-
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-Vue.use(VueRouter);
-
-import App from './App.vue';
-import router from './routes.js';
-
-const app = new Vue({
-    el: '#root',
-    router,
-    render: h => h(App),
-});
-
 // require('./bootstrap');
-
 // window.Vue = require('vue');
 
-// import router from './routes.js'
+// import Vue from 'vue';
+// import VueRouter from 'vue-router';
+// Vue.use(VueRouter);
 
 // import App from './App.vue';
+// import router from './routes.js';
 
 // const app = new Vue({
 //     el: '#root',
 //     router,
 //     render: h => h(App),
 // });
+
+require('./bootstrap');
+
+window.Vue = require('vue').default;
+
+import router from './routes.js'
+
+import App from './App.vue';
+
+const app = new Vue({
+    el: '#root',
+    router,
+    render: h => h(App),
+});
 
 
