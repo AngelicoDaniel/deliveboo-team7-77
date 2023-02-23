77242088f489a946c0113ea679c6aa83a69b53a1
Daniel <angelicodaniel93@gmail.com>
2023-02-23 10:20:40 +0100

    inizio routes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,15 @@
 require('./bootstrap');
 
-// window.Vue = require('vue');
-import Vue from 'vue';
+window.Vue = require('vue');
+// import Vue from 'vue';
 
 import App from './App.vue';
+import router from './routes.js';
+
 
 const app = new Vue({
     el: '#root',
+    router,
     render: h => h(App),
 });
 
