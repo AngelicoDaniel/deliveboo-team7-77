eb63836d279c4d5fc3ef4db49027e92589824952
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-02-23 17:19:13 +0100

    Filtro ristoranti

--- resources/js/components/TypeCard.vue
+++ resources/js/components/TypeCard.vue
@@ -1,48 +1,48 @@
+
+
 <template>
-  <div class="text-center">
-    <h1 class="m-4">{{ this.$route.params.name }}</h1>
-     <!-- <ul>
-      <li v-for="elem in type" :key="elem.id">
+    <div class="text-center">
+        <h1 class="m-4">{{ this.$route.params.name }}</h1>
+
         <ul>
-          <li v-for="user in elem.users" :key="user.id">
-            {{ user.name }}
-          </li>
+            <li v-for="(elem, index) in type" :key="index">
+                <ul>
+                    <li v-for="(user, index) in elem.users" :key="index">
+                        {{ user.name }}
+                    </li>
+                </ul>
+            </li>
         </ul>
-      </li>
-    </ul> -->
-
-  </div>
-
+</div>
 </template>
 
 <script>
 export default {
-  name: "TypeCard",
-  components: {},
-  data() {
-    return {
-      type: [],
-    };
-  },
-  mounted() {
-    this.getType();
-  },
-  methods: {
-    getType() {
-      axios
-        .get("http://127.0.0.1:8000/api/types" + this.$route.params.name)
-        .then((res) => {
-
-          this.type = res;
-          console.log(this.type );
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+    name: "TypeCard",
+    components: {},
+    data() {
+        return {
+            type: [],
+        };
     },
-  },
+    mounted() {
+        this.getType();
+    },
+    methods: {
+        getType() {
+            axios
+                .get("http://127.0.0.1:8000/api/types/" + this.$route.params.name)
+                .then((res) => {
+                    this.type = res.data;
+                    console.log(this.type);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        },
+    },
 };
 </script>
 
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
+
