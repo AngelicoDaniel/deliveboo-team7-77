7de185f9710e4ecf467989dc8ec5c2fba6ab77f5
Daniel <angelicodaniel93@gmail.com>
2023-02-24 17:10:38 +0100

    search

--- resources/js/components/TypeComp.vue
+++ resources/js/components/TypeComp.vue
@@ -1,55 +1,50 @@
 <template>
     <div>
-        <input type="text" v-model="search" placeholder="Search restaurant type" />
-        <div>
-            <ul>
-                <li v-for="elem in filteredTypes" :key="elem.id">
-                    <router-link class="nav-link active" aria-current="page" :to="`/types/${elem.name}`">{{ elem.name
-                    }}</router-link>
-                </li>
-            </ul>
-        </div>
-</div>
-</template>
+      <input type="text" v-model="search" placeholder="Search restaurant type">
+      <div>
+        <ul>
+          <li v-for="elem in filteredTypes" :key="elem.id">
+            <router-link class="nav-link active" aria-current="page" :to="`/types/${elem.name}`">{{ elem.name }}</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </template>
 
 <script>
 import axios from 'axios';
 
 export default {
-    name: 'TypeComp',
+  name: 'TypeComp',
 
-    mounted() {
-        this.getTypes();
-    },
+  mounted() {
+    this.getTypes();
+  },
 
-    data() {
-        return {
-            types: [],
-            search: '',
-            originalTypes: [],
-        };
-    },
+  data() {
+    return {
+      types: [],
+      search: '',
+      originalTypes: []
+    }
+  },
 
-    methods: {
-        getTypes() {
-            axios
-                .get('http://127.0.0.1:8000/api/types')
-                .then((res) => {
-                    this.types = res.data;
-                    this.originalTypes = res.data;
-                })
-                .catch((error) => {
-                    console.log(error);
-                });
-        },
-    },
+  methods: {
+    getTypes() {
+      axios.get('http://127.0.0.1:8000/api/types')
+        .then((res) => {
+          this.types = res.data;
+          this.originalTypes = res.data;
+        })
+    }
+  },
 
-    computed: {
-        filteredTypes() {
-            return this.types.filter((type) => {
-                return type.name.toLowerCase().includes(this.search.toLowerCase());
-            });
-        },
-    },
-};
+  computed: {
+    filteredTypes() {
+      return this.types.filter((type) => {
+        return type.name.toLowerCase().includes(this.search.toLowerCase());
+      });
+    }
+  }
+}
 </script>
