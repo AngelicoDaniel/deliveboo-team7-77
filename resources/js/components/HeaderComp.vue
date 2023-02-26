4e4bf79f139c0f8cdce7191db8597cda67f43203
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-02-26 11:34:04 +0100

    Aggiunto logo deliveroo

--- resources/js/components/HeaderComp.vue
+++ resources/js/components/HeaderComp.vue
@@ -1,26 +1,27 @@
 <template>
     <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <router-link class="navbar-brand" :to="{ name: 'home' }">Navbar</router-link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-light " data-bs-theme="dark">
+            <div class="container-fluid bg-dark d-flex justify-content-between">
+                <div class="">
+                    <router-link class="navbar-brand" :to="{ name: 'home' }"><img class="img-logo"
+                            src="../../../public/images/6103fbd57410870004d7f2dd.png" alt=""></router-link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+
+                <div class="collapse navbar-collapse " id="navbarNav ">
+                    <ul class="navbar-nav ">
                         <li class="nav-item">
                             <router-link class="nav-link active" aria-current="page"
                                 :to="{ name: 'home' }">Home</router-link>
                         </li>
-                        <!-- <li class="nav-item">
-                                <router-link class="nav-link" :to="{ name: 'types' }">Type Listllllllllll</router-link>
-                            </li> -->
+
+
                         <li class="nav-item">
                             <a class="nav-link" href="http://127.0.0.1:8000/admin">Area Riservataa</a>
                         </li>
-
-
                     </ul>
                 </div>
             </div>
@@ -34,3 +35,9 @@ export default {
     name: 'Header'
 }
 </script>
+<style>
+.img-logo {
+
+    width: 170px;
+}
+</style>
