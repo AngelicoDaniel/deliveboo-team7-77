2683e5b12c46651be4e7e9d8d85128473c30cba3
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2023-03-01 12:21:16 +0100

    Aggiornato immagini

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -15,37 +15,37 @@ class UserSeeder extends Seeder
     public function run(Faker $faker)
     {
         $restaurants = [
-            [
-                'name'       =>  'McDonald',
-                'email'      =>  'mcdonald@libero.it',
-                'password'   =>   Hash::make('mcdonald'),
-                'address'    =>   $faker->address(),
-                'PIVA'       =>   $faker->randomNumber(9, true),
-                'image_logo' =>   'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
-            ],
-            [
-                'name'       =>  'BurgerKing',
-                'email'      =>  'burgerking@libero.it',
-                'password'   =>   Hash::make('BurgerKing'),
-                'address'    =>   $faker->address(),
-                'PIVA'       =>   $faker->randomNumber(9, true),
-                'image_logo' =>   'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
-            ],
-            [
-                'name'       =>  'Kfc',
-                'email'      =>  'kfc@libero.it',
-                'password'   =>   Hash::make('Kfc'),
-                'address'    =>   $faker->address(),
-                'PIVA'       =>   $faker->randomNumber(9, true),
-                'image_logo' =>   'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
-            ],
+            // [
+            //     'name'       =>  'McDonald',
+            //     'email'      =>  'mcdonald@libero.it',
+            //     'password'   =>   Hash::make('mcdonald'),
+            //     'address'    =>   $faker->address(),
+            //     'PIVA'       =>   $faker->randomNumber(9, true),
+            //     'image_logo' =>   'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
+            // ],
+            // [
+            //     'name'       =>  'BurgerKing',
+            //     'email'      =>  'burgerking@libero.it',
+            //     'password'   =>   Hash::make('BurgerKing'),
+            //     'address'    =>   $faker->address(),
+            //     'PIVA'       =>   $faker->randomNumber(9, true),
+            //     'image_logo' =>   'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
+            // ],
+            // [
+            //     'name'       =>  'Kfc',
+            //     'email'      =>  'kfc@libero.it',
+            //     'password'   =>   Hash::make('Kfc'),
+            //     'address'    =>   $faker->address(),
+            //     'PIVA'       =>   $faker->randomNumber(9, true),
+            //     'image_logo' =>   'https://picsum.photos/id/'. rand(0, 1000) .'/500/400',
+            // ],
             [
                 "name" => "O'Pizzaiuolo",
                 "email" => "pizzaiuolo@live.it",
                 "password" => 'pizzaiuolo',
                 "address" => 'Via Toledo, 8 - Napoli',
                 "PIVA" => '12548951657',
-                "image_logo" => 'image_logos/restaurants/pizzaiuolo.jpg',
+                "image_logo" => 'images/restaurants/pizzaiuolo.jpg',
             ],
             [
                 "name" => "Pizzeria da Ciro",
@@ -54,7 +54,7 @@ class UserSeeder extends Seeder
                 "address" => 'Via Napoli, 8 - Napoli',
                 "PIVA" => '15548956657',
                 "description" => 'La pizza più buona di Napoli',
-                "image_logo" => 'image_logos/restaurants/daciro.jpg',
+                "image_logo" => 'images/restaurants/daciro.jpg',
             ],
             [
                 "name" => "Hokkaido",
@@ -63,16 +63,16 @@ class UserSeeder extends Seeder
                 "address" => 'Piazza Giuseppe Verdi, 15 - Napoli',
                 "PIVA" => '85416378925',
                 "description" => 'Barche di sushi e tempura, anche nella formula all you can eat, in locale con cucina a vista e arredi minimal.',
-                "image_logo" => 'image_logos/restaurants/hokkaido.jpg',
+                "image_logo" => 'images/restaurants/hokkaido.jpg',
             ],
-        [
+            [
                 "name" => "Streat Piombo",
                 "email" => "piombostrt@gmail.com",
                 "password" => 'piombo00',
                 "address" => 'Via Sebastiano del Piombo, 12, Napoli',
                 "PIVA" => '69542031025',
                 "description" => 'Ristorante Americano, con una vasta gamma di piatti, da tutte le epoche.',
-                "image_logo" => 'image_logos/restaurants/piombo.jpg',
+                "image_logo" => 'images/restaurants/piombo.jpg',
             ],
             [
                 "name" => "Gluten Free",
@@ -81,7 +81,7 @@ class UserSeeder extends Seeder
                 "address" => 'Via Milano, 8 - Napoli',
                 "PIVA" => '15587956887',
                 "description" => 'Il cibo gluten free più buono di tutti',
-                "image_logo" => 'image_logos/restaurants/gluten.jpg',
+                "image_logo" => 'images/restaurants/gluten.jpg',
             ],
             [
                 "name" => "Hang Zhou",
@@ -90,16 +90,16 @@ class UserSeeder extends Seeder
                 "address" => 'Via Principe Eugenio, 82, Napoli',
                 "PIVA" => '10000568487',
                 "description" => "Specialità cinesi servite in porzioni abbondanti e abbellite da decorazioni di verdure.",
-                "image_logo" => 'image_logos/restaurants/zhou.jpg',
+                "image_logo" => 'images/restaurants/zhou.jpg',
             ],
-        [
+            [
                 "name" => "Hachi Ristorante Giapponese",
                 "email" => "hachi@gmail.com",
                 "password" => 'hachi000',
                 "address" => 'Via Luca Giordano 33, Napoli ',
                 "PIVA" => '95100236654',
                 "description" => 'Ristorante giapponese',
-                "image_logo" => 'image_logos/restaurants/hachi.jpg',
+                "image_logo" => 'images/restaurants/hachi.jpg',
             ],
             [
                 "name" => "La Scimmietta",
@@ -108,7 +108,7 @@ class UserSeeder extends Seeder
                 "address" => 'Via Giovanni Amendola, 176/178, Napoli',
                 "PIVA" => '10855688879',
                 "description" => "Semifreddi, gelati artigianali e gusti originali serviti in un ambiente dal clima allegro e pareti colorate.",
-                "image_logo" => 'image_logos/restaurants/scimmietta.jpg',
+                "image_logo" => 'images/restaurants/scimmietta.jpg',
             ],
             [
                 "name" => "Poke House",
@@ -117,16 +117,16 @@ class UserSeeder extends Seeder
                 "address" => 'Via Aurno, 8 - Napoli',
                 "PIVA" => '78855689979',
                 "description" => 'Che pokè!',
-                "image_logo" => 'image_logos/restaurants/pokehouse.jpg',
+                "image_logo" => 'images/restaurants/pokehouse.jpg',
             ],
-        [
+            [
                 "name" => "Cavoli Nostri",
                 "email" => "nostricavoli@gmail.com",
                 "password" => 'cavolituoi',
                 "address" => 'Via Palepoli, 32, Napoli ',
                 "PIVA" => '46006998521',
                 "description" => 'Niente carne e pesce, solo cavoli.',
-                "image_logo" => 'image_logos/restaurants/cavolinostri.jpg',
+                "image_logo" => 'images/restaurants/cavolinostri.jpg',
             ],
             [
                 "name" => "Shabnaz",
@@ -135,7 +135,7 @@ class UserSeeder extends Seeder
                 "address" => 'Via Liborio Romano, 19, Napoli',
                 "PIVA" => '87624159320',
                 "description" => "Shabnaz l'indiano più famoso di tutti i tempi di Lecce",
-                "image_logo" => 'image_logos/restaurants/shabnaz.jpg',
+                "image_logo" => 'images/restaurants/shabnaz.jpg',
             ],
             [
                 "name" => "The Mexican",
@@ -144,7 +144,7 @@ class UserSeeder extends Seeder
                 "address" => 'Via Palermo, 8 - Napoli',
                 "PIVA" => '78858887979',
                 "description" => 'Tacos e tortillas',
-                "image_logo" => 'image_logos/restaurants/themexican.jpg',
+                "image_logo" => 'images/restaurants/themexican.jpg',
             ],
         ];
 
