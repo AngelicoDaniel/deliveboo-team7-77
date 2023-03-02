8d6ff5c7760222714d65fa8ac9d3d0d39c908a6a
Daniel <angelicodaniel93@gmail.com>
2023-03-02 10:29:50 +0100

    dish card hover

--- resources/js/components/DishCard.vue
+++ resources/js/components/DishCard.vue
@@ -1,130 +1,152 @@
 <template>
-  <div class="text-center">
-    <div class="row justify-content-center my-5">
-      <h3 class="text-center">Seleziona il tuo piatto preferito</h3>
-    </div>
+    <div class="text-center">
+      <div class="row justify-content-center my-5">
+        <h3 class="text-center">Seleziona il tuo piatto preferito</h3>
+      </div>
 
-    <div class="">
-      <div class="card-deck d-flex justify-content-center">
-        <div
-          v-for="(elem, index) in dish"
-          :key="index"
-          class="card"
-          :style="{ backgroundImage: `url(/storage/${elem.image})` }"
-        >
-          <div class="card-overlay">
-            <h5 class="card-title">{{ elem.name }}</h5>
-            <p class="card-text">{{ elem.price }}</p>
-            <p class="card-text">{{ elem.description }}</p>
-            <button
-              @click="addCart(elem.name, elem.price, elem.id, elem.user_id)"
-              class="btn btn-primary"
-            >
-              Aggiungi al Carrello
-            </button>
+      <div class="">
+        <div class="card-deck d-flex justify-content-center">
+          <div
+            v-for="(elem, index) in dish"
+            :key="index"
+            class="card"
+            :style="{ backgroundImage: `url(/storage/${elem.image})` }"
+          >
+            <div class="card-overlay">
+              <h5 class="card-title">{{ elem.name }}</h5>
+              <p class="card-text">{{ elem.price }}</p>
+              <p class="card-text">{{ elem.description }}</p>
+              <button
+                @click="addCart(elem.name, elem.price, elem.id, elem.user_id)"
+                class="btn btn-primary"
+              >
+                Aggiungi al Carrello
+              </button>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</template>
+  </template>
 
-<script>
-export default {
-  name: "DishCard",
+  <script>
+  export default {
+    name: "DishCard",
 
-  data() {
-    return {
-      dish: [],
-      cart: [],
-    };
-  },
+    data() {
+      return {
+        dish: [],
+        cart: [],
+      };
+    },
 
-  mounted() {
-    this.getDish();
+    mounted() {
+      this.getDish();
 
-    localStorage
-      .getItem("cart")
-      .split(",")
-      .forEach((element) => {
-        this.cart.push(element);
-      });
-  },
-  methods: {
-    getDish() {
-      axios
-        .get("http://127.0.0.1:8000/api/dishes/" + this.$route.params.user_id)
-        .then((res) => {
-          this.dish = res.data;
-          console.log(this.dish);
-        })
-        .catch((err) => {
-          console.log(err);
+      localStorage
+        .getItem("cart")
+        .split(",")
+        .forEach((element) => {
+          this.cart.push(element);
         });
     },
+    methods: {
+      getDish() {
+        axios
+          .get("http://127.0.0.1:8000/api/dishes/" + this.$route.params.user_id)
+          .then((res) => {
+            this.dish = res.data;
+            console.log(this.dish);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
 
-    addCart(name, price, id, user_id) {
-      if (this.cart.length > 0 && user_id != this.cart[0].user_id) {
-        alert("Non Puoi inserire piatti di altri ristoranti nel carrello!!");
-      } else {
-        this.cart.push({
-          id: id,
-          name: name,
-          price: price,
-          user_id: user_id,
-        }),
-          localStorage.setItem("cart", JSON.stringify(this.cart));
-        alert("Piatto aggiunto al carrello!");
-      }
+      addCart(name, price, id, user_id) {
+        if (this.cart.length > 0 && user_id != this.cart[0].user_id) {
+          alert("Non Puoi inserire piatti di altri ristoranti nel carrello!!");
+        } else {
+          this.cart.push({
+            id: id,
+            name: name,
+            price: price,
+            user_id: user_id,
+          }),
+            localStorage.setItem("cart", JSON.stringify(this.cart));
+          alert("Piatto aggiunto al carrello!");
+        }
+      },
+
+      removeCart() {
+        this.cart = [];
+        this.totalPrice = 0;
+        localStorage.removeItem("cart");
+        localStorage.removeItem("priceCart");
+      },
     },
-
-    removeCart() {
-      this.cart = [];
-      this.totalPrice = 0;
-      localStorage.removeItem("cart");
-      localStorage.removeItem("priceCart");
-    },
-  },
-};
-</script>
+  };
+  </script>
 
 
-<style scoped lang="scss">
+  <style scoped lang="scss">
 
-.card {
-  flex-basis: 22%;
-  justify-content: space-between;
-  margin: 10px 10px;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  aspect-ratio: 1/1;
-}
+  .card {
+    flex-basis: 22%;
+    justify-content: space-between;
+    margin: 10px 10px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    aspect-ratio: 1/1;
+    position: relative; /* needed for absolute positioning of .card-overlay */
+  }
 
-.card-deck {
-  display: flex;
-  flex-wrap: wrap;
-}
+  .card-deck {
+    display: flex;
+    flex-wrap: wrap;
+  }
 
-.card-titl .card-tex {
-  color: white;
-}
+  .card-titl .card-tex {
+    color: white;
+  }
 
-.card-overlay {
-  margin-top: 25%;
-  padding: 10px;
-  background-color: rgba(255, 255, 255, 0.3);
-  text-align: center;
-  color: black;
-}
+  .card-overlay {
+    opacity: 0; /* hide content initially */
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+    padding: 10px;
+    background-color: rgba(255, 255, 255, 0.3);
+    text-align: center;
+    color: black;
+    transition: opacity 0.2s ease-in-out; /* add a transition effect */
+  }
 
-.card-title {
-  font-size: 1.2rem;
-  margin: 0;
-}
+  .card:hover .card-overlay {
+    opacity: 1; /* show content on hover */
+  }
 
-.card-text {
-  font-size: 1rem;
-  margin: 0;
-}
-</style>
+  .card-title {
+    font-size: 1.2rem;
+    margin: 0;
+  }
+
+  .card-text {
+    font-size: 1rem;
+    margin: 0;
+  }
+
+  .card-title {
+    font-size: 1.2rem;
+    margin: 0;
+  }
+
+  .card-text {
+    font-size: 1rem;
+    margin: 0;
+  }
+  </style>
