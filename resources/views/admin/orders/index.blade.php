a0191be6b70bc331d0d0797fb112fb326f6af4fa
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-03-06 10:09:31 +0100

    ordini

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,29 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Ordini:</h1>
 
-        @if (count($orders) > 0)
+    <div id="user_index" class="row flex-column justify-content-center mb-5">
+        <h1 class="col-12 text-center p-3 mb-5 shadow">
+            I TUOI ORDINI
+        </h1>
+
+        @if ($orders->count() > 0)
             @foreach ($orders as $order)
-                <div class="row">
-                    <ul class="col-5">
-                        <a href="{{ route('admin.orders.show', ['order' => $order]) }}" class="btn btn-outline-info">Info</a>
-                        <li>
-                           Numero Ordine: {{ $order->order_number }}
-                        </li>
-                        <li>
-                           Data ordine: {{ $order->created_at }}
-                        </li>
-                        <li>
-                            Prezzo totale: {{ $order->ship_cost }} €
-                        </li>
-                    </ul>
+                <div class="card m-3 shadow">
+
+                    <div class="card-body row">
+                        <div class="col-md-12">
+                            <h4>Ordine n. {{ $order->order_number  }}</h4>
+                            <hr>
+                            <p class="card-title">Nome e cognome cliente: {{ $order->customer_name }}
+                                {{ $order->customer_surname }}</p>
+                            <p class="card-title">Indirizzo: {{ $order->customer_address }}</p>
+
+                            <p class="card-title">Telefono: {{ $order->customer_phone }}</p>
+                            <p class="card-title">Email: {{ $order->customer_email }}</p>
+                            <p class="card-title">Totale: €{{ $order->ship_cost }}</p>
+                        </div>
+                        {{-- <div class="col-md-6">
+                            <h4 class="mt-md-0 mt-3">Piatti ordinati</h4>
+                            <hr>
+
+                            <div>
+                                @foreach ($order->plate as $plate)
+                                    <p class="card-title">{{ $plate->pivot->quantity }}x {{ $plate->name }} -
+                                        €{{ $plate->pivot->quantity * $plate->price }}</p>
+                                    <div class="md-0">
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div> --}}
+                    </div>
                 </div>
             @endforeach
         @else
-            <p>No orders found.</p>
+            <h4 class="text-center">Non hai ancora nessun ordine.</h4>
         @endif
-
     </div>
 @endsection
