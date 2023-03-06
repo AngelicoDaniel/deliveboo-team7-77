d264d5d9af4344e2b1b5bcd33bc2839d35073560
Andrea-Lulli <andrea.lulli89@gmail.com>
2023-03-06 11:52:28 +0100

    search

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -117,12 +117,11 @@
                                 </div>
 
                             </li>
-
-                            <form action="{{ Request::url() }}" method="GET" class="d-flex">
+                            {{-- <form action="{{ Request::url() }}" method="GET" class="d-flex">
                                 <input class="form-control me-2" type="search" name="search" placeholder="Search"
                                     aria-label="Search">
                                 <button class="btn btn-outline-success" type="submit">Search</button>
-                            </form>
+                            </form> --}}
                         @endguest
                     </ul>
                 </div>
